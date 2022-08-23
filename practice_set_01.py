6dd63d2fc3310a047a6fffd7671a2f3b7c76c7c5
Aditya Chandrakant Mahajan <90857611+AdityaCM65@users.noreply.github.com>
2022-08-23 19:09:37 +0530

    Practice set on Modules , Comments and Pip
    Chapter : 1 Modules , Comments and pip .
    Practice set 1 Solved questions

---
+++ practice_set_01.py
@@ -0,0 +1,33 @@
+# Practice Set : Chapter 1
+
+# 1 : Print "Twinkle Twinkle Little Star" Poem.
+
+# print('''Twinkle Twinkle Little Star
+# How I Wonder What You are!
+# Up Above The World So high,
+# Like a Diamond In the Sky.''')
+
+
+# 2 : Use REPL ( Read Evalute Print Loop) and Print the table of 5 using it.
+
+# 3 : Install an external module and use it to perform an operation of your interest.
+# ->  >>> pip install playsound
+        #  from playsound import playsound
+        #  playsound('path of file')
+
+# 4 : Write a python program to print contents of a directory using os module , search online for the function which does that.
+# import os
+# print(os.listdir())
+
+# 5 : Label the program written in problem 4 with comments.
+# -> 
+    #  Author : ACM
+    #  Location : Mars
+    #  Date : 13/08/2022
+
+
+
+
+
+
+
