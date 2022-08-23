dd29f5847d6f246ee9d53969342b03d9d703ece5
Aditya Chandrakant Mahajan <90857611+AdityaCM65@users.noreply.github.com>
2022-08-23 19:11:48 +0530

    Practice Set on Variables and Datatypes
    Chapter : 2 Variables and Datatypes.
    Practice set 2 Solved Programs

---
+++ practice_set_02.py
@@ -0,0 +1,69 @@
+# Practice Set : Chapter 2
+
+# 1 : Write a python program to add two numbers.
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# sum = num1 + num2;
+# print("Sum : " , sum)
+
+# 2 : Write a python program to find remainder when a number is divided by 2.
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# print("Remainder is : ", (num1 % num2))
+
+# 3 : Check the type of the variable assigned using input() function.
+# n = input("Enter Here ")
+# print(type(n))
+
+# 4 : Use comparison Operator to find out whether a given variable 'a' is greater tha  'b' or not.
+#     Take a = 34 and b = 80
+
+# a = 34
+# b = 80
+
+# c = (a>b)
+# print(c)
+
+# - With User Input and Conditional Statement: 
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+
+# if(num1 > num2) :
+#     print(num1 , " is Greater than " , num2)
+# else:
+#     print(num2," is Greater than " , num1)
+
+# 5 : Write a python program to find average of two numbers entered by the user.
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# avg = (num1 + num2) / 2
+# print("Average Of Two Numbers : ", avg)
+
+# 6 : Write a python program to calculate square of a number entered by user.
+# num1 = int(input("Enter a Number : "))
+# sqr = num1 * num1
+# print("Square of ", num1 , " is : " , sqr)
+
+# Extra : Use All Arithmetic Operators - 
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+
+# sum = num1 + num2
+# mul = num1 * num2
+# div = num1 / num2
+# sub = num1 - num2
+# mod = num1 % num2
+
+# print("Addition : " , sum)
+# print("Multiplication : " , mul)
+# print("Substraction : " , sub)
+# print("Division : " , div)
+# print("Remainder : " , mod)
+
+  
+
+
+
